005e9b1cd93a46de20aad6d19dc94eb0c8cce091
carlos hernandez <youvlog01gmail.com>
2025-06-09 11:57:44 -0600

    TABLA

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 import './Layout.css';
+import { useLocation } from 'react-router-dom';
 
 const Layout = ({ children, pages }) => {
+  const location = useLocation();
+  // Oculta el sidebar si la ruta es de producto
+  const hideSidebar = location.pathname.startsWith('/producto/');
+
   return (
     <div className="layout">
-      <Sidebar pages={pages} />
-      <main className="main-content">
+      {!hideSidebar && <Sidebar pages={pages} />}
+      <main className="main-content" style={hideSidebar ? { marginLeft: 0 } : {}}>
         {children}
       </main>
     </div>
   );
 };
 
-export default Layout; 
+export default Layout;
