894a411c84326ad900e2a86c42939a3cc8de071a
carlos hernandez <youvlog01gmail.com>
2025-11-25 14:06:44 -0600

    contacto

--- src/pages/Contacto.js
+++ src/pages/Contacto.js
@@ -50,8 +50,17 @@ const Contacto = () => {
           </div>
         </div>
       </div>
+
+      <a href="https://wa.me/5219333251889" className="whatsapp-icon" target="_blank" rel="noopener noreferrer">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+          <path fill="none" d="M0 0h24v24H0z" />
+          <path d="M12 2C6.48 2 2 6.48 2 12c0 2.21.73 4.26 1.95 5.92l-1.43 4.43a1 1 0 0 0 1.24 1.24l4.43-1.43A11.93 11.93 0 0 0 22 12c0-5.52-4.48-10-10-10zm-.5 17.79V20h1v-1.21A8.978 8.978 0 0 0 18 12c0-4.97-4.03-9-9-9S1 7.03 1 12c0 4.97 4.03 9 9 9a8.978 8.978 0 0 0 1.5-.21z" />
+          <path d="M15.5 10h-7v1h7v-1zm0 3h-7v1h7v-1z" />
+        </svg>
+      </a>
     </div>
   );
 };
 
 export default Contacto;
+
