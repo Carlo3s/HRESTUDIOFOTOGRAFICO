8ccc8ec6fbb5ec889c899fd31bcdc4edff603a15
carlos hernandez <youvlog01gmail.com>
2025-11-25 14:22:33 -0600

    mj

--- src/App.js
+++ src/App.js
@@ -1,30 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Sidebar from './components/Sidebar';
-import HomePage from './pages/HomePage';
-import ManglarVerde from './pages/ManglarVerde';
-import ManglarBlanco from './pages/ManglarBlanco';
-import ManglarRojo from './pages/ManglarRojo';
-import QuienesSomos from './pages/QuienesSomos';
-import AcercaDe from './pages/AcercaDe';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import Contacto from './pages/Contacto';
-import './index.css';
+import './styles/Responsive.css';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Sidebar />
-      <main className="app-main">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/manglar-verde" element={<ManglarVerde />} />
-          <Route path="/manglar-blanco" element={<ManglarBlanco />} />
-          <Route path="/manglar-rojo" element={<ManglarRojo />} />
-          <Route path="/quienes-somos" element={<QuienesSomos />} />
-          <Route path="/acerca-de" element={<AcercaDe />} />
-          <Route path="/contacto" element={<Contacto />} />
-        </Routes>
-      </main>
+      <div className="app-wrapper">
+        <Navbar />
+        <main className="app-main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contacto" element={<Contacto />} />
+            {/* Otras rutas */}
+          </Routes>
+        </main>
+      </div>
     </Router>
   );
 }
